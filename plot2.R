e6898c9a4ac0e3b63cd34ac080060032b8ef1447
drabbit17 <matteo.pallini17@gmail.com>
2015-05-21 18:27:26 +0200

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -3,10 +3,10 @@ elect <- read.table("household_power_consumption.txt", header = TRUE, sep = ";",
 
 library(dplyr)
 
-elect$Date <- as.Date(elect$Date, format="%d/%m/%Y")
-welect <- filter(elect, Date=="2007-02-01" | Date=="2007-02-02")
-welect$date <- strptime(paste(welect$Date, welect$Time), format="%Y-%m-%d %H:%M:%S")
+elect$Date <- as.Date(elect$Date, format = "%d/%m/%Y")
+welect <- filter(elect, Date == "2007-02-01" | Date == "2007-02-02")
+welect$date <- strptime(paste(welect$Date, welect$Time), format = "%Y-%m-%d %H:%M:%S")
 
 png(filename = "plot1.png", width = 480, height = 480)
-        plot(welect$date, welect$Global_active_power,type="l" , ylab="Global Active Powe(Kilowatts)",xlab="")
+        plot(welect$date, welect$Global_active_power, type = "l", ylab = "Global Active Powe(Kilowatts)", xlab = "")
 dev.off()
